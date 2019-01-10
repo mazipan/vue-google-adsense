0274b3dae83986838e049d5e1e8cf6c55036c6f3
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-01-10 12:07:22 +0000

    Deploy mazipan/vue-google-adsense to github.com/mazipan/vue-google-adsense.git:gh-pages

---
+++ precache-manifest.00d4c4acc5914604261395a923f27426.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fc4cbbea0da1a8c61f590775b0f8521d",
+    "url": "/vue-google-adsense/index.html"
+  },
+  {
+    "revision": "910bc48d7b1ce5111b4f",
+    "url": "/vue-google-adsense/assets/js/runtime~index.910bc48d.js"
+  },
+  {
+    "revision": "6a965deda190e38619ba",
+    "url": "/vue-google-adsense/assets/js/index.6a965ded.chunk.js"
+  },
+  {
+    "revision": "51bbfae3c4b8bbaad2a7",
+    "url": "/vue-google-adsense/assets/js/chunk-vendors.51bbfae3.chunk.js"
+  },
+  {
+    "revision": "6a965deda190e38619ba",
+    "url": "/vue-google-adsense/assets/css/index.6a965ded.chunk.css"
+  }
+];
--- sw.js
+++ sw.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/vue-google-adsense/precache-manifest.b4439afa0e9e23b5a070a63ce4279568.js"
+  "/vue-google-adsense/precache-manifest.00d4c4acc5914604261395a923f27426.js"
 );
 
 /**
