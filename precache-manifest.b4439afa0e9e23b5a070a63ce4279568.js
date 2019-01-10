10878798066b77657a6089a5a9af47e7dbb15762
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-01-10 11:58:06 +0000

    Deploy mazipan/vue-google-adsense to github.com/mazipan/vue-google-adsense.git:gh-pages

---
+++ precache-manifest.b4439afa0e9e23b5a070a63ce4279568.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "df7af5963860f37ff4a990fb327a27ec",
+    "url": "/vue-google-adsense/index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/vue-google-adsense/images/logo.png"
+  },
+  {
+    "revision": "910bc48d7b1ce5111b4f",
+    "url": "/vue-google-adsense/assets/js/runtime~index.910bc48d.js"
+  },
+  {
+    "revision": "fd865f5c16e9b6f6f128",
+    "url": "/vue-google-adsense/assets/js/index.fd865f5c.chunk.js"
+  },
+  {
+    "revision": "51bbfae3c4b8bbaad2a7",
+    "url": "/vue-google-adsense/assets/js/chunk-vendors.51bbfae3.chunk.js"
+  },
+  {
+    "revision": "fd865f5c16e9b6f6f128",
+    "url": "/vue-google-adsense/assets/css/index.fd865f5c.chunk.css"
+  }
+];
---
+++ sw.js
@@ -0,0 +1,27 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/vue-google-adsense/precache-manifest.b4439afa0e9e23b5a070a63ce4279568.js"
+);
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
