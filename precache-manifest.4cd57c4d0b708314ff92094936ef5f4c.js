39d0825de82be2a263368efa5a5dfa57f4d612ea
Irfan Maulana <irfan.maulana@bizzy.co.id>
2019-03-28 13:25:12 +0700

    Publishing folder demo to gh-pages in 3/28/2019, 1:24:53 PM

--- precache-manifest.4cd57c4d0b708314ff92094936ef5f4c.js
+++
@@ -1,9 +0,0 @@
-self.__precacheManifest = (self.__precacheManifest || []).concat([
-  {
-    "revision": "4ff93d8c1648025409bc638d51116e1c",
-    "url": "/vue-google-adsense/index.html"
-  },
-  {
-    "url": "/vue-google-adsense/app.0645965285e5d513f2eb.js"
-  }
-]);
