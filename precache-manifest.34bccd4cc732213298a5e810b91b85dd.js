dbcbe6b9f02c23e2fc3408f18e8df2ac25a28894
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-09-23 14:02:58 +0000

    Deploy mazipan/vue-google-adsense to github.com/mazipan/vue-google-adsense.git:gh-pages

---
+++ precache-manifest.34bccd4cc732213298a5e810b91b85dd.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e67cce29a2a8ecf148f34114bbafb85e",
+    "url": "/vue-google-adsense/android-chrome-192x192.png"
+  },
+  {
+    "revision": "2f3dc08dd23bef5d7d7ef697247ed2a9",
+    "url": "/vue-google-adsense/android-chrome-512x512.png"
+  },
+  {
+    "revision": "d83ab1a310ae480b7059635bee30425e",
+    "url": "/vue-google-adsense/apple-touch-icon.png"
+  },
+  {
+    "revision": "b7dcd768c6bf7a45f44e",
+    "url": "/vue-google-adsense/css/app.d738855a.css"
+  },
+  {
+    "revision": "74ccf4b7d5423271f203",
+    "url": "/vue-google-adsense/css/in-article.d738855a.css"
+  },
+  {
+    "revision": "6820331f4eaf801e2f69",
+    "url": "/vue-google-adsense/css/in-feed.d738855a.css"
+  },
+  {
+    "revision": "88ca479ff41e620c9c3b3b02dbf67f15",
+    "url": "/vue-google-adsense/favicon-16x16.png"
+  },
+  {
+    "revision": "959fe82d13df471fe0454c4cbac58091",
+    "url": "/vue-google-adsense/favicon-32x32.png"
+  },
+  {
+    "revision": "6f06aa2ef09cccc9447ed33fa698ce00",
+    "url": "/vue-google-adsense/index.html"
+  },
+  {
+    "revision": "b7dcd768c6bf7a45f44e",
+    "url": "/vue-google-adsense/js/app.e9c58c83.js"
+  },
+  {
+    "revision": "9054efc73ab281cebb21",
+    "url": "/vue-google-adsense/js/chunk-vendors.fcca1fcc.js"
+  },
+  {
+    "revision": "74ccf4b7d5423271f203",
+    "url": "/vue-google-adsense/js/in-article.d5bb01bb.js"
+  },
+  {
+    "revision": "6820331f4eaf801e2f69",
+    "url": "/vue-google-adsense/js/in-feed.69f92a06.js"
+  },
+  {
+    "revision": "897a18074612fcc2a6788208f3a18668",
+    "url": "/vue-google-adsense/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue-google-adsense/robots.txt"
+  },
+  {
+    "revision": "8ef2e9e97dd457cfc88bef14442a06a5",
+    "url": "/vue-google-adsense/site.webmanifest"
+  }
+]);
