eb2fb49a502aead4076c760739a969e4087f4467
James Harrison <james@fasttortoise.co.uk>
2022-02-15 15:37:02 +0000

    Fix Eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,7 +8,7 @@ module.exports = {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-tabs': 'off',
-    "@typescript-eslint/ban-ts-comment": "off",
+    '@typescript-eslint/ban-ts-comment': 'off',
   },
   parserOptions: {
     parser: '@typescript-eslint/parser',
